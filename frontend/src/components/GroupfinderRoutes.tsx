22a858ece50d58232e38c8a77648b1fc1bdec4a2
qegabe <qegabe@gmail.com>
2023-06-12 11:22:37 -0400

    changed some routes to require login

--- frontend/src/components/GroupfinderRoutes.tsx
+++ frontend/src/components/GroupfinderRoutes.tsx
@@ -1,23 +1,33 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import Home from "./Home";
 import Register from "./user/Register";
 import Login from "./user/Login";
 import Groups from "./group/Groups";
 import NewGroupForm from "./group/NewGroupForm";
 import GroupDetail from "./group/GroupDetail";
+import { useAppSelector } from "../hooks/redux";
 
 function GroupfinderRoutes() {
+  const user = useAppSelector((s) => s.auth.user);
+
   return (
     <Routes>
       <Route path="/" element={<Home />} />
 
       <Route path="/groups" element={<Groups />} />
-      <Route path="/groups/new" element={<NewGroupForm />} />
-      <Route path="/groups/:id" element={<GroupDetail />} />
-
-      <Route path="/register" element={<Register />} />
-      <Route path="/login" element={<Login />} />
+      {user.username ? (
+        <>
+          <Route path="/groups/new" element={<NewGroupForm />} />
+          <Route path="/groups/:id" element={<GroupDetail />} />
+        </>
+      ) : (
+        <>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+        </>
+      )}
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
 }
